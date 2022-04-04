80e6628e4cb538fcb69b74c06f94beded7585e99
MedNoun <mouhasahss@gmail.com>
2022-04-04 22:50:22 +0100

    creating a customized http exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter())
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, //removes the non listed
