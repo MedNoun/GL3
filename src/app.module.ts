616dbf777c964afacf445ad34c3b80c877aefe8b
MedNoun <mouhasahss@gmail.com>
2022-04-04 21:41:23 +0100

    async in the app module will let the type orm module load after config module is resolved

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,21 +9,23 @@ import appConfig from './config/app.config';
 
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        autoLoadEntities: true, //load modules automatically instead of specifying the entities array
+        synchronize: true, //automatically generates a SQL table from all classes with @entity decorator but only for development only disable it in production
+        //synchronizes the typeORM entities with the database every time
+      }),
+    }),
     ConfigModule.forRoot({
       load: [appConfig],
     }),
     CoffeesModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true, //load modules automatically instead of specifying the entities array
-      synchronize: true, //automatically generates a SQL table from all classes with @entity decorator but only for development only disable it in production
-      //synchronizes the typeORM entities with the database every time
-    }),
   ],
   controllers: [AppController],
   providers: [AppService],
