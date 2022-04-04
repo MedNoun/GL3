d6bc2b37a65b58ed0ad4ff0b57ef08bdb022aa16
MedNoun <mouhasahss@gmail.com>
2022-04-04 23:19:22 +0100

    creating an interceptor with rxjs methods allowing us to wrap the incoming request or response with what we see fits

---
+++ src/common/interceptors/wrap-response.interceptor.ts
@@ -0,0 +1,16 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { map, Observable, tap } from 'rxjs';
+
+@Injectable()
+export class WrapResponseInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    console.log('beforee ....');
+
+    return next.handle().pipe(map((data) => ({ data })));
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { ApiKeyGuard } from './common/guards/api-key.guard';
+import { WrapResponseInterceptor } from './common/interceptors/wrap-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new WrapResponseInterceptor());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, //removes the non listed
