5563ab0ad71cbdeac91aad291501cca2c8bef1aa
MedNoun <mouhasahss@gmail.com>
2022-04-04 22:58:31 +0100

    adding auth guard

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { ApiKeyGuard } from './common/guards/api-key.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new HttpExceptionFilter())
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalGuards(new ApiKeyGuard());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, //removes the non listed
