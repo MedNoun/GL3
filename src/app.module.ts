078bf8773b02f010e1c90c1ce0410940d8a6bf21
MedNoun <mouhasahss@gmail.com>
2022-04-04 21:24:32 +0100

    config files

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,12 @@ import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from '@hapi/joi';
+import appConfig from './config/app.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      validationSchema: Joi.object({
-        DATABASE_HOST: Joi.required(),
-        DATABASE_PORT: Joi.number().default(5432),
-      }),
+      load: [appConfig],
     }),
     CoffeesModule,
     TypeOrmModule.forRoot({
